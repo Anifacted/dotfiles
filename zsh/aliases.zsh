e35b7f5d3d0b9a0092458b02d14654a6274625bf
Marc Høegh <mhoegh90@gmail.com>
2023-09-21 14:14:58 +0200

    Add aliases for the git command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,10 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias world='zdi services restart && zdi apps restart'
+
+# I can accidentally type 't' before 'i'
+alias gti="git"        
+
+# I can accidentally type 'u' or 'o' which are neighbors to 'i'
+alias got="git"   
+alias gut="git"
